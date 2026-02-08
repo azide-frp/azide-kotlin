8eb390c7f27d20b1aea5f10edf2a48dfe1241212
Jakub Trzebiatowski <jakub.trzebiatowski+git@proton.me>
2026-02-08 20:08:02 +0100

    `EventStream.executeEach` tests: Migrate to the stimulation banks

--- core/src/commonTest/kotlin/dev/azide/core/event_stream/EventStream_sampleEach_reaction_tests.kt
+++ core/src/commonTest/kotlin/dev/azide/core/event_stream/EventStream_sampleEach_reaction_tests.kt
@@ -4,23 +4,39 @@ import dev.azide.core.EventStream
 import dev.azide.core.Moment
 import dev.azide.core.sampleEach
 import dev.azide.core.sampling
-import dev.azide.core.test_utils.TestSlottedStimulationScenario1x2
-import dev.azide.core.test_utils.TestSlottedStimulationScenario2x2
-import dev.azide.core.test_utils.bind
-import dev.azide.core.test_utils.cell.TestInputCell
 import dev.azide.core.test_utils.event_stream.EventStream_expectations_testUtils
+import dev.azide.core.test_utils.event_stream.EventStream_generic_testUtils
+import dev.azide.core.test_utils.event_stream.EventStream_generic_testUtils.SourceEventStreamTag
 import dev.azide.core.test_utils.event_stream.EventStream_reaction_testUtils
 import dev.azide.core.test_utils.event_stream.TestInputEventStream
 import dev.azide.core.test_utils.event_stream.correctingEmission
+import dev.azide.core.test_utils.event_stream.emitting
 import dev.azide.core.test_utils.event_stream.revokingEmission
+import dev.azide.core.test_utils.cell.TestInputCell
 import dev.azide.core.test_utils.generic.ExpectedTestSubjectReaction.IntermediatePropagationTolerance
+import dev.azide.core.test_utils.stimulation_combinatorics.TestSlotCount
+import dev.azide.core.test_utils.stimulation_combinatorics.TestSlottedStimulationScenario
+import dev.azide.core.test_utils.stimulation_combinatorics.bind
 import kotlin.test.Test
 
-@Suppress("ClassName")
+@Suppress("ClassName", "PrivatePropertyName")
 class EventStream_sampleEach_reaction_tests {
+    private typealias SuitableSlotCount = TestSlotCount.Count2
+
+    private typealias SuitableTestSlottedStimulationScenario = TestSlottedStimulationScenario<SuitableSlotCount>
+
+    private val slottedStimulationBank_sourceEventStreamEmits =
+        EventStream_generic_testUtils.stimulationBank_sourceEventStreamEmits.distribute(slotCount = SuitableSlotCount)
+
+    private val slottedStimulationBank_sourceEventStreamEmitsRevoked =
+        EventStream_generic_testUtils.stimulationBank_sourceEventStreamEmitsRevoked.distribute(slotCount = SuitableSlotCount)
+
+    private val slottedStimulationBank_sourceEventStreamEmitsCorrected =
+        EventStream_generic_testUtils.stimulationBank_sourceEventStreamEmitsCorrected.distribute(slotCount = SuitableSlotCount)
+
     @Test
     fun test_step_sourceEmits() {
-        TestSlottedStimulationScenario1x2.entries.forEach { slottedStimulationScenario ->
+        slottedStimulationBank_sourceEventStreamEmits.forEach { slottedStimulationScenario ->
             test_step_sourceEmits(
                 slottedStimulationScenario = slottedStimulationScenario,
             )
@@ -28,7 +44,7 @@ class EventStream_sampleEach_reaction_tests {
     }
 
     private fun test_step_sourceEmits(
-        slottedStimulationScenario: TestSlottedStimulationScenario1x2,
+        slottedStimulationScenario: SuitableTestSlottedStimulationScenario,
     ) {
         val helperCell = TestInputCell(initialValue = 10)
 
@@ -38,7 +54,8 @@ class EventStream_sampleEach_reaction_tests {
 
         EventStream_reaction_testUtils.executeReactionTransaction(
             subjectEventStream = subjectEventStream,
-            slottedInputStimulation = sourceEventStream.emit(
+            slottedInputStimulation = sourceEventStream.emitting(
+                tag = SourceEventStreamTag,
                 emittedEvent = helperCell.sampling,
             ).bind(slottedStimulationScenario),
             expectedSubjectEmission = EventStream_expectations_testUtils.expectEmission(
@@ -49,7 +66,7 @@ class EventStream_sampleEach_reaction_tests {
 
     @Test
     fun test_step_sourceEmitsRevoked() {
-        TestSlottedStimulationScenario2x2.entries.forEach { slottedStimulationScenario ->
+        slottedStimulationBank_sourceEventStreamEmitsRevoked.forEach { slottedStimulationScenario ->
             test_step_sourceEmitsRevoked(
                 slottedStimulationScenario = slottedStimulationScenario,
             )
@@ -57,7 +74,7 @@ class EventStream_sampleEach_reaction_tests {
     }
 
     private fun test_step_sourceEmitsRevoked(
-        slottedStimulationScenario: TestSlottedStimulationScenario2x2,
+        slottedStimulationScenario: SuitableTestSlottedStimulationScenario,
     ) {
         val helperCell = TestInputCell(initialValue = 10)
 
@@ -68,6 +85,7 @@ class EventStream_sampleEach_reaction_tests {
         EventStream_reaction_testUtils.executeReactionTransaction(
             subjectEventStream = subjectEventStream,
             slottedInputStimulation = sourceEventStream.revokingEmission(
+                tag = SourceEventStreamTag,
                 emittedEvent = helperCell.sampling,
             ).bind(slottedStimulationScenario),
             expectedSubjectEmission = EventStream_expectations_testUtils.expectNoEmission(
@@ -78,7 +96,7 @@ class EventStream_sampleEach_reaction_tests {
 
     @Test
     fun test_step_sourceEmitsCorrected() {
-        TestSlottedStimulationScenario2x2.entries.forEach { slottedStimulationScenario ->
+        slottedStimulationBank_sourceEventStreamEmitsCorrected.forEach { slottedStimulationScenario ->
             test_step_sourceEmitsCorrected(
                 slottedStimulationScenario = slottedStimulationScenario,
             )
@@ -86,7 +104,7 @@ class EventStream_sampleEach_reaction_tests {
     }
 
     private fun test_step_sourceEmitsCorrected(
-        slottedStimulationScenario: TestSlottedStimulationScenario2x2,
+        slottedStimulationScenario: SuitableTestSlottedStimulationScenario,
     ) {
         val helperCell1 = TestInputCell(initialValue = 10)
         val helperCell2 = TestInputCell(initialValue = 20)
@@ -98,6 +116,7 @@ class EventStream_sampleEach_reaction_tests {
         EventStream_reaction_testUtils.executeReactionTransaction(
             subjectEventStream = subjectEventStream,
             slottedInputStimulation = sourceEventStream.correctingEmission(
+                tag = SourceEventStreamTag,
                 intermediateEmittedEvent = helperCell1.sampling,
                 correctedEmittedEvent = helperCell2.sampling,
             ).bind(slottedStimulationScenario),
