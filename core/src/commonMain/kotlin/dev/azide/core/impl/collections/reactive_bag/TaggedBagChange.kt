fd6f8f41ee48888b46bdba0ea49401e0ce004c6b
Jakub Trzebiatowski <jakub.trzebiatowski+git@proton.me>
2026-02-15 13:47:08 +0100

    `TaggedBagChange`: Improve TODO markers

--- core/src/commonMain/kotlin/dev/azide/core/impl/collections/reactive_bag/TaggedBagChange.kt
+++ core/src/commonMain/kotlin/dev/azide/core/impl/collections/reactive_bag/TaggedBagChange.kt
@@ -27,14 +27,14 @@ data class TaggedBagChange<out ElementT>(
     }
 
     override val addedElements: Collection<ElementT>
-        get() = TODO("Not yet implemented")
+        get() = TODO("addedElements")
 
     override val removedElements: Collection<ElementT>
-        get() = TODO("Not yet implemented")
+        get() = TODO("removedElements")
 
     fun filter(
         predicate: (ElementT) -> Boolean,
-    ): TaggedBagChange<ElementT>? = TODO()
+    ): TaggedBagChange<ElementT>? = TODO("TaggedBagChange.filter")
 
     override val sizeDelta: Int
         get() = addedElements.size - removedElements.size
