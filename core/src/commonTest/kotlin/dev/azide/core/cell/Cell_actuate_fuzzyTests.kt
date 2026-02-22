0238b47e57b778fd91253583b728a1e71c64bf35
Jakub Trzebiatowski <jakub.trzebiatowski+git@proton.me>
2026-02-22 22:07:28 +0100

    Add stubs for some fuzzy tests

---
+++ core/src/commonTest/kotlin/dev/azide/core/cell/Cell_actuate_fuzzyTests.kt
@@ -0,0 +1,6 @@
+package dev.azide.core.cell
+
+@Suppress("ClassName")
+class Cell_actuate_fuzzyTests {
+    // TODO: Implement fuzzy tests
+}
---
+++ core/src/commonTest/kotlin/dev/azide/core/cell/Cell_executeEvery_fuzzyTests.kt
@@ -0,0 +1,6 @@
+package dev.azide.core.cell
+
+@Suppress("ClassName")
+class Cell_executeEvery_fuzzyTests {
+    // TODO: Implement fuzzy tests
+}
