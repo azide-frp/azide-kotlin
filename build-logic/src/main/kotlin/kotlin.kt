b0925ba1b1947bc029461125f634fc7a7d01a7a8
Jakub Trzebiatowski <jakub.trzebiatowski+git@proton.me>
2026-02-24 17:57:22 +0100

    Configure JS test tasks with a timeout duration

--- build-logic/src/main/kotlin/kotlin.kt
+++ build-logic/src/main/kotlin/kotlin.kt
@@ -1,6 +1,9 @@
 import dev.kmpx.gradle.kotlin.dsl.utils.ExperimentalLanguageFeature
 import dev.kmpx.gradle.kotlin.dsl.utils.experimentalLanguageFeatures
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsSubTargetDsl
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 // Java 21 is the most recent LTS version
 private const val usedJvmToolchainVersion = 21
@@ -12,6 +15,8 @@ private val enabledExperimentalCompilerFeatures = listOf(
     ExperimentalLanguageFeature.NestedTypeAliases,
 )
 
+val testTimeoutDuration: Duration = 10.seconds
+
 fun KotlinMultiplatformExtension.configureKotlinCompilerOptions() {
     compilerOptions {
         experimentalLanguageFeatures.addAll(enabledExperimentalCompilerFeatures)
@@ -22,11 +27,30 @@ fun KotlinMultiplatformExtension.configureKotlin() {
     jvm()
 
     js(IR) {
-        browser()
-        nodejs()
+        browser {
+            testTask(
+                timeoutDuration = testTimeoutDuration,
+            )
+        }
+
+        nodejs {
+            testTask(
+                timeoutDuration = testTimeoutDuration,
+            )
+        }
     }
 
     jvmToolchain(usedJvmToolchainVersion)
 
     configureKotlinCompilerOptions()
+}
+
+private fun KotlinJsSubTargetDsl.testTask(
+    timeoutDuration: Duration,
+) {
+    testTask {
+        useMocha {
+            timeout = "${timeoutDuration.inWholeSeconds}s"
+        }
+    }
 }
